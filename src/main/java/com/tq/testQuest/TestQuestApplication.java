a0956a604a264d320175f2c82b706442e5c32327
aleksej <dr1ma@icloud.com>
2023-09-01 17:48:42 +0600

    fix db bag

--- src/main/java/com/tq/testQuest/TestQuestApplication.java
+++ src/main/java/com/tq/testQuest/TestQuestApplication.java
@@ -1,4 +1,4 @@
-package com.tq.testQuest.services;
+package com.tq.testQuest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
